3d30352515110b0c00c5c85a39b255f3c69d7c52
Choisujin <98chltnwls@gmail.com>
2022-12-12 14:32:37 +0900

    Array

---
+++ src/edu/kh/array/ex/ArrayRun.java
@@ -0,0 +1,16 @@
+package edu.kh.array.ex;
+
+public class ArrayRun {
+
+	public static void main(String[] args) {
+		
+		ArrayExample1 ArryEx = new ArrayExample1();
+		
+		
+		//ArryEx.ex1();
+		//ArryEx.ex2();
+		//ArryEx.ex3();
+		ArryEx.ex4();
+	}
+
+}
