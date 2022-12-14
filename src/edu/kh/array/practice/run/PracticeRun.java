9dd149bba3354864ba0f312543e72e04e372defc
Choisujin <98chltnwls@gmail.com>
2022-12-14 15:17:51 +0900

    Array

--- src/edu/kh/array/practice/run/PracticeRun.java
+++ src/edu/kh/array/practice/run/PracticeRun.java
@@ -15,9 +15,9 @@ public class PracticeRun {
 		//practice.practice5();
 		//practice.practice6();
 		//practice.practice7();
-		practice.practice8();
+		//practice.practice8();
 		//practice.practice9();
-		//practice.practice10();
+		practice.practice10();
 		//practice.practice11();
 		//practice.practice12();
 		//practice.practice13();
--- src/edu/kh/array/practice/service/PracticeService.java
+++ src/edu/kh/array/practice/service/PracticeService.java
@@ -203,6 +203,116 @@ public class PracticeService {
 		}		
 	}
 	
+	public void practice8() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			System.out.print("정수: ");
+			int input = sc.nextInt();
+			
+			if (input < 3 || input % 2 ==0) {
+				System.out.println("다시 입력하세요.");
+				
+			} else {
+				int arr[] = new int[input];
+				
+				int num = 0;
+			
+				for (int i = 0; i < arr.length; i++) {
+					if ( i <= arr.length / 2) {
+						arr[i] = ++num;
+					
+					} else {
+						arr[i] = --num;
+					}
+					
+					// 출력 시, 추가 (단, 마지막 제외)
+					if (i == arr.length - 1) {
+						System.out.print(arr[i]);
+					}else {
+						System.out.print(arr[i] + ",");
+					}
+				} 
+				break;
+			}
+			
+		}	
+	}
 	
+	public void practice9() {
+	/* 10개의 값을 저장할 수 있는 정수형 배열을 선언 및 할당하고,
+	1~10 사이의 난수를 발생시켜 배열에 초기화한 후 출력하세요.
+
+	[실행 화면]
+	발생한 난수 : 9 7 6 2 5 10 7 2 9 6 */
+	
+		int arr[] = new int[10];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int)(Math.random() * 10 +1);		
+		}
+	
+		System.out.print("발생한 난수: ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	
+	
+	
+	}	
+
+	public void practice10() {
+		/*
+		10개의 값을 저장할 수 있는 정수형 배열을 선언 및 할당하고,
+		1~10 사이의 난수를 발생시켜 배열에 초기화 후
+		배열 전체 값과 그 값 중에서 최대값과 최소값을 출력하세요.			
+		[실행 화면]
+		발생한 난수 : 5 3 2 7 4 8 6 10 9 10	 			
+		최대값 : 10
+		최소값 : 2
+		 */
+		int arr[] = new int[10];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = (int)(Math.random() * 10 + 1);
+		}
+		System.out.print("발생한 난수: ");
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+		
+		int max = 1;
+		int min = 10;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+			}
+			
+			if(arr[i] < min) {
+				min = arr[i];
+			}
+			
+		}
+		System.out.println("최대값: " + max);
+		System.out.println("최소값: " + min);
+		
+		
+	}
+
+	public void practice11() {
+		/*
+		 * 10개의 값을 저장할 수 있는 정수형 배열을 선언 및 할당하고
+			1~10 사이의 난수를 발생시켜 중복된 값이 없게 배열에 초기화한 후 출력하세요.
+			
+			[실행 화면]
+			4 1 3 6 9 5 8 10 7 2
+		 */
+		
+		
+	}
 
 }
